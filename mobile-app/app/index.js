cac9c0f919eb992380444cf536aabf635c281cc0
Emmanuel Kyeremeh <josephkyeremeh53@gmail.com>
2023-08-22 21:46:10 +0000

    added tf.js

--- mobile-app/app/index.js
+++ mobile-app/app/index.js
@@ -6,7 +6,6 @@ import {
   TouchableOpacity,
   Alert,
   ActivityIndicator,
-  Button,
 } from "react-native";
 import { Stack } from "expo-router";
 import React, { useEffect, useState } from "react";
@@ -20,7 +19,37 @@ import { Ionicons } from "@expo/vector-icons";
 import * as ImagePicker from "expo-image-picker";
 import * as FileSystem from "expo-file-system";
 import Prediction from "../components/PredictionPage";
-// import Tflite from "tflite-react-native";
+import * as tf from "@tensorflow/tfjs";
+import { bundleResourceIO, decodeJpeg } from "@tensorflow/tfjs-react-native";
+
+export const class_names = [
+  "Bacterial spot of bell Pepper",
+  "Healthy Bell Pepper",
+  "Bacterial spot of Tomato",
+  "Early blight of Tomato",
+  "Late blight of Tomato",
+  "Leaf mold of Tomato",
+  "Septoria leaf spot of Tomato",
+  "Spider mites or Two-spotted spider mite of Tomato",
+  "Target Spot of Tomato",
+  "Yellow leaf curl virus of Tomato",
+  "Mosaic virus of Tomato",
+  "Healthy Tomato",
+];
+
+export const loadModel = async () => {
+  try {
+    const model = await tf.loadGraphModel(
+      bundleResourceIO(modelJSON, modelWeights)
+    );
+    return model;
+  } catch (error) {
+    console.log(`Error loading model ${error}`);
+  }
+};
+
+export const modelJSON = require("../model/model.json");
+export const modelWeights = require("../model/group1-shard1of1.bin");
 
 const Home = () => {
   const [startCamera, setStartCamera] = useState(false);
@@ -30,22 +59,51 @@ const Home = () => {
   const [showPrediction, setShowPrediction] = useState(false);
   const predictionRoute = "http://192.168.45.90:8080/predict";
 
-  // let tflite = new Tflite();
+  const getPredictions = async (uri) => {
+    try {
+      await tf.ready();
+      await tf.setBackend("cpu");
+      const img64 = await FileSystem.readAsStringAsync(uri, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+      const imgBuffer = tf.util.encodeString(img64, "base64").buffer;
+      const raw = new Uint8Array(imgBuffer);
+      let imgTensor = decodeJpeg(raw);
 
-  // let model_tflite = require("../model/model_version_1.tflite");
-  // let labels_file = require("../model/model_version_1.txt");
+      const scalar = tf.scalar(255);
 
-  // tflite.loadModel(
-  //   {
-  //     model: model_tflite, // required
-  //     labels: labels_file, // required
-  //     numThreads: 1, // defaults to 1
-  //   },
-  //   (err, res) => {
-  //     if (err) console.log(err);
-  //     else console.log(res);
-  //   }
-  // );
+      imgTensor = tf.image.resizeNearestNeighbor(imgTensor, [224, 224]);
+
+      const tensorScaled = imgTensor.div(scalar);
+
+      const img = tf.reshape(tensorScaled, [1, 224, 224, 3]);
+
+      const model = await loadModel();
+      const result = model.predict(img);
+      const prediction = await result.data();
+      let max = prediction[0];
+      let idx = 0;
+
+      for (let i = 0; i < prediction.length; i++) {
+        if (prediction[i] > max) {
+          max = prediction[i];
+          idx = i;
+        }
+      }
+      console.log(`Prediction is ${class_names[idx]}`);
+      console.log(`Confidence is ${max}`);
+
+      let accuracy = parseFloat(max) * 100.0;
+
+      setConfidence(`${accuracy.toFixed(2)}%`);
+      setPrediction(class_names[idx]);
+      setLoading(false);
+      setShowPrediction(true);
+    } catch (error) {
+      console.log(`The Error is: ${error}`);
+      setLoading(false);
+    }
+  };
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -59,33 +117,25 @@ const Home = () => {
       setLoading(true);
 
       let uri = result.assets[0].uri;
+      getPredictions(uri);
 
-      // tflite.runModelOnImage(
-      //   {
-      //     path: uri, // required
-      //   },
-      //   (err, res) => {
-      //     if (err) console.log(err);
-      //     else console.log(res);
-      //   }
-      // );
-      try {
-        const res = await FileSystem.uploadAsync(predictionRoute, uri, {
-          fieldName: "file",
-          httpMethod: "POST",
-          uploadType: FileSystem.FileSystemUploadType.MULTIPART,
-        });
-        let predictionObject = JSON.parse(res.body);
-        let accuracy = parseFloat(predictionObject.confidence) * 100.0;
+      // try {
+      //   const res = await FileSystem.uploadAsync(predictionRoute, uri, {
+      //     fieldName: "file",
+      //     httpMethod: "POST",
+      //     uploadType: FileSystem.FileSystemUploadType.MULTIPART,
+      //   });
+      //   let predictionObject = JSON.parse(res.body);
+      //   let accuracy = parseFloat(predictionObject.confidence) * 100.0;
 
-        setConfidence(`${accuracy.toFixed(2)}%`);
-        setPrediction(predictionObject.class);
-        setLoading(false);
-        setShowPrediction(true);
-      } catch (error) {
-        setLoading(false);
-        console.log(error);
-      }
+      //   setConfidence(`${accuracy.toFixed(2)}%`);
+      //   setPrediction(predictionObject.class);
+      //   setLoading(false);
+      //   setShowPrediction(true);
+      // } catch (error) {
+      //   setLoading(false);
+      //   console.log(error);
+      // }
     }
   };
 
@@ -102,27 +152,13 @@ const Home = () => {
 
   const _takePicture = async () => {
     if (!camera) return;
+    await _startCamera();
+    if (startCamera) {
+      const photo = await camera.takePictureAsync();
+      setStartCamera(false);
+      setLoading(true);
 
-    const photo = await camera.takePictureAsync();
-    setStartCamera(false);
-    setLoading(true);
-
-    try {
-      const res = await FileSystem.uploadAsync(predictionRoute, photo.uri, {
-        fieldName: "file",
-        httpMethod: "POST",
-        uploadType: FileSystem.FileSystemUploadType.MULTIPART,
-      });
-      let predictionObject = JSON.parse(res.body);
-      let accuracy = parseFloat(predictionObject.confidence) * 100.0;
-
-      setConfidence(`${accuracy.toFixed(2)}%`);
-      setPrediction(predictionObject.class);
-      setLoading(false);
-      setShowPrediction(true);
-    } catch (error) {
-      setLoading(false);
-      console.log(error);
+      getPredictions(photo.uri);
     }
   };
 
